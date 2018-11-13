204ae4a50720541c02eab835f2034eed94310a05
Zak Burke <zburke@cornell.edu>
2018-11-13 08:43:53 -0500

    lint

--- About.js
+++ About.js
@@ -29,7 +29,13 @@ class About extends React.Component {
     const dk = this.props.dataKey || '';
     let greeting;
     if (this.props.resources.greetingParams) {
-      greeting = <h3>{this.props.resources.greetingParams.greeting} {this.props.resources.greetingParams.name}</h3>;
+      greeting = (
+        <h3>
+          {this.props.resources.greetingParams.greeting}
+          { }
+          {this.props.resources.greetingParams.name}
+        </h3>
+      );
     } else {
       greeting = <h3>No one here :(</h3>;
     }
@@ -37,8 +43,16 @@ class About extends React.Component {
       <div className={css.root}>
         {greeting}
         <form onSubmit={this.handleSubmit}>
-          <label className={css.label} htmlFor={`g${dk}`}>Greeting:</label> <input className={css.textInput} id={`g${dk}`} type="text" />
-          <label className={css.label} htmlFor={`n${dk}`}>Person:</label> <input className={css.textInput} id={`n${dk}`} type="text" />
+          <label className={css.label} htmlFor={`g${dk}`}>
+            Greeting:
+            { }
+            <input className={css.textInput} id={`g${dk}`} type="text" />
+          </label>
+          <label className={css.label} htmlFor={`n${dk}`}>
+            Person:
+            { }
+            <input className={css.textInput} id={`n${dk}`} type="text" />
+          </label>
           <button className={css.button} type="submit">Greet</button>
         </form>
       </div>
